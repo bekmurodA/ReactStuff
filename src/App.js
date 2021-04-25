f1cc563b6a5709b63533eb16a479b44a6bfd44c6
bekmurodA <7248774american@gmail.com>
2021-04-25 22:39:54 +0500

    installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from  'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const welcome = {
+	greeting:'Hey',
+	title:'React',
+};
+
+const list = [
+	{
+		title:'React',
+		url:'https://reactjs.org/',
+		author:'Jordan Walke',
+		num_comments: 3,
+		points: 4,
+		objectID: 0,
+	},
+	{
+		title: 'Redux',
+		url:'https://redux.js.org/',
+		author:'Dan abramov, Andrew Clark',
+		num_contents:2,
+		points:5,
+		objectID:1
+	},
+];
+function swear(name){
+	return ('Love you '+ name)
+}
+function App(){
+	return(
+	   <div>
+		<h1>My Hacker Stories </h1>
+		<label htmlFor="search">Search: </label>
+		<input id="search" type="text"/>
+		<hr/>
+		{list.map(function(item){
+			return <div>{item.title}</div>;
+		})}
+	   </div>
+	
+	);
 }
 
 export default App;
